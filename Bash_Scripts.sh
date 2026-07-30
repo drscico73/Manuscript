7e0daa47b5072ab42c07010c82c7fd4b466e6fbd
Prerna Goel <107932131+GoelPrerna@users.noreply.github.com>
2026-07-30 15:49:01 +0200

    Remove redundant filtering commands from script

--- Bash_Scripts.sh
+++ Bash_Scripts.sh
@@ -57,32 +57,3 @@ awk 'NR > 1 {printf $1":"$2":"$3":"$4}' hel_marker_file.txt|sort -u > hel_marker
 #Prepare merged sample file
 bcftools annotate --set-id '%CHROM:%POS:%REF:%ALT' -Oz -o mpileup_flt_ids.vcf.gz mpileup_flt.vcf.gz
 bcftools annotate --set-id '%CHROM:%POS:%REF:%ALT' -Oz -o marker_flt_ids.vcf.gz marker_flt.vcf.gz
-
-#Filtering
-
-bcftools view -i 'ID=@ore_marker_ids.txt' -Oz -o ore_mpileup_flt.vcf.gz mpileup_flt_ids.vcf.gz
-bcftools view -i 'ID=@hel_marker_ids.txt' -Oz -o hel_mpileup_flt.vcf.gz mpileup_flt_ids.vcf.gz
-bcftools view -i 'ID=@sam_marker_ids.txt' -Oz -o sam_mpileup_flt.vcf.gz mpileup_flt_ids.vcf.gz
-bcftools view -i 'ID=@ore_marker_ids.txt' -Oz -o ore_marker_flt.vcf.gz marker_flt_ids.vcf.gz
-bcftools view -i 'ID=@hel_marker_ids.txt' -Oz -o hel_marker_flt.vcf.gz marker_flt_ids.vcf.gz
-bcftools view -i 'ID=@sam_marker_ids.txt' -Oz -o sam_marker_flt.vcf.gz marker_flt_ids.vcf.gz
-
-### Preparing sample-specific files ###
-
-bcftools query -l mpileup_flt.vcf.gz | grep -E '^HO-|^Dmel_Helsinki_inbred_F11$|^Dmel_OregonR_non-inbred$' > ho_samples.txt
-bcftools query -l mpileup_flt.vcf.gz | grep -E '^HS-|^Dmel_Helsinki_inbred_F11$|^Dmel_Samarkand_inbred$' > hs_samples.txt
-bcftools query -l mpileup_flt.vcf.gz | grep -E '^SO-|^Dmel_Samarkand_inbred$|^Dmel_OregonR_non-inbred$' > so_samples.txt
-
-bcftools view -S ho_samples.txt -Oz -o hel_ho_mpileup.vcf.gz hel_mpileup_flt.vcf.gz 
-bcftools view -S hs_samples.txt -Oz -o hel_hs_mpileup.vcf.gz hel_mpileup_flt.vcf.gz
-bcftools view -S so_samples.txt -Oz -o ore_so_mpileup.vcf.gz ore_mpileup_flt.vcf.gz
-bcftools view -S ho_samples.txt -Oz -o ore_ho_mpileup.vcf.gz ore_mpileup_flt.vcf.gz
-bcftools view -S hs_samples.txt -Oz -o sam_hs_mpileup.vcf.gz sam_mpileup_flt.vcf.gz
-bcftools view -S so_samples.txt -Oz -o sam_so_mpileup.vcf.gz sam_mpileup_flt.vcf.gz
-
-bcftools view -s ^Dmel_Samarkand_inbred -Oz -o hel_ho_markers.vcf.gz hel_marker_flt.vcf.gz
-bcftools view -s ^Dmel_OregonR_non-inbred -Oz -o hel_hs_markers.vcf.gz hel_marker_flt.vcf.gz
-bcftools view -s ^Dmel_OregonR_non-inbred -Oz -o sam_hs_markers.vcf.gz sam_marker_flt.vcf.gz
-bcftools view -s ^Dmel_Helsinki_inbred_F11 -Oz -o sam_so_markers.vcf.gz sam_marker_flt.vcf.gz
-bcftools view -s ^Dmel_Helsinki_inbred_F11 -Oz -o ore_so_markers.vcf.gz ore_marker_flt.vcf.gz
-bcftools view -s ^Dmel_Samarkand_inbred -Oz -o ore_ho_markers.vcf.gz ore_marker_flt.vcf.gz
